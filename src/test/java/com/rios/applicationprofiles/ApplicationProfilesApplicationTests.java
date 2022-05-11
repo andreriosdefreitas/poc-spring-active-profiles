082f5fe2544ef8ee544e89d8935afe94e037e20f
Andre Rios de Freitas <andre.f@tamanna.com>
2022-05-12 00:34:44 +0100

    @ActiveProfiles({default, it}) in it and it/application-it.yml and @ActiveProfiles({default, test}) in it and test/application-test.yml

--- src/test/java/com/rios/applicationprofiles/ApplicationProfilesApplicationTests.java
+++ src/test/java/com/rios/applicationprofiles/ApplicationProfilesApplicationTests.java
@@ -3,8 +3,10 @@ package com.rios.applicationprofiles;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"default", "test"})
 class ApplicationProfilesApplicationTests {
 
 	@Test
