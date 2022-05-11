2a77db15b1f41c69aa52520faa878b6626010d70
Andre Rios de Freitas <andre.f@tamanna.com>
2022-05-12 00:26:46 +0100

    @ActiveProfiles({default, it}) in it and it/application-it.yml

--- src/it/java/com/rios/applicationprofiles/ApplicationProfilesApplicationIT.java
+++ src/it/java/com/rios/applicationprofiles/ApplicationProfilesApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles({"default", "test"})
+@ActiveProfiles({"default", "it"})
 class ApplicationProfilesApplicationIT {
 
 	@Test
