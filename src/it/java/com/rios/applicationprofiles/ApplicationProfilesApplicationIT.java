0e744068d50794739a42a57b5669c6b3ce185667
Andre Rios de Freitas <andre.f@tamanna.com>
2022-06-03 14:23:26 +0100

    bkp

--- src/it/java/com/rios/applicationprofiles/ApplicationProfilesApplicationIT.java
+++ src/it/java/com/rios/applicationprofiles/ApplicationProfilesApplicationIT.java
@@ -1,10 +1,12 @@
 package com.rios.applicationprofiles;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Slf4j
 @SpringBootTest
 @ActiveProfiles({"default", "it"})
 class ApplicationProfilesApplicationIT {
@@ -12,6 +14,7 @@ class ApplicationProfilesApplicationIT {
 	@Test
 	@DisplayName("contextLoadsIt")
 	void contextLoadsIt() {
+		log.info("it");
 	}
 
 }
